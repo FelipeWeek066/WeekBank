24b5f7dc11301f17b645a6f4a699f3760866de33
FelipeWeek066 <felipe1smg1g@gmail.com>
2026-06-11 19:44:06 +0100

    first commit

---
+++ src/test/java/com/weeklab/weekbank/WeekbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weeklab.weekbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ weekbank/src/main/java/com/weeklab/weekbank/WeekbankApplication.java
@@ -0,0 +1,13 @@
+package com.weeklab.weekbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekbankApplication.class, args);
+	}
+
+}
